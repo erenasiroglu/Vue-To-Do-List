e56d0d7700e79a0f5a4ae58bd1ae27cca6b53127
Eren Nasıroğlu <101004391+erenasiroglu@users.noreply.github.com>
2023-01-31 10:06:53 +0300

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,3 @@
-<!-- <template>
-  <div class="home">
-    <Website></Website>
-  </div>
-</template>
-
-<script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import WebSite from "@/components/WebSite/WebSite.vue";
-
-@Component({
-  components: {
-    WebSite,
-  },
-})
-export default class AboutView extends Vue {}
-</script> -->
-
 <template>
   <div class="home">
     <todo-list></todo-list>
